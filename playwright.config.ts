84e89efaa16a22ac2acf124b63324965c8d7fbdb
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-07-19 17:33:14 -0600

    Added "/// <reference types="node" />" to top of playwright.config.ts to remove typescript typing errors for "process".
    Renamed "example.spec.ts" to "todoMVCDemo.spec.ts".

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,10 @@
+/// <reference types="node" />
+
+/* The above line is an explicit pointer to node type definitions in the node_modules/@types folder.
+Prior to adding this the "process" lines had type definition errors.
+You could also take care of this in a tsconfig.json file global definition
+but this project currently does not use one. */
+
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -26,7 +33,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://demo.playwright.dev/todomvc/#/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,9 +78,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'https://demo.playwright.dev/todomvc/#/',
+    reuseExistingServer: !process.env.CI,
+  },
 });
